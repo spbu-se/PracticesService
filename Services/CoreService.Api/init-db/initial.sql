f6e4d68244f0f06421c642700c8af6dee6ac6a8d
Belgrak <glekar03@mail.ru>
2025-06-02 09:46:19 +0300

    Fixed not null constraint for Practice

--- Services/CoreService.Api/init-db/initial.sql
+++ Services/CoreService.Api/init-db/initial.sql
@@ -61,8 +61,8 @@ CREATE TABLE Practices
 (
     Id SERIAL PRIMARY KEY,
     StudentId INT NOT NULL,
-    ConsultantId INT NOT NULL,
-    SupervisorId INT,
+    ConsultantId INT,
+    SupervisorId INT NOT NULL,
     ThemeId INT NOT NULL,
     Type VARCHAR(255) NOT NULL, 
     FinalGrade VARCHAR(5),
